5141cb28f558fab0538c73f7f2092688b9cc1c05
Sunil Baghel <sunilbaghel93100@gmail.com>
2025-08-04 20:36:59 +0530

    improvements

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/postcss'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/postcss"; // Use @tailwindcss/postcss
 
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist', // Confirm output directory
+    outDir: "dist", // Output directory
   },
   css: {
     postcss: {
-      plugins: [tailwindcss()],
+      plugins: [tailwindcss(), require("autoprefixer")],
     },
   },
-  base: './', // ⬅️ very important
-})
+  base: "./", // Relative paths for assets
+});
